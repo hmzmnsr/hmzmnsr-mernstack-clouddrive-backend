aeed7a8681e3439e81d0a6291c1b483acaa16a45
Hamza <hamzamansoora@gmail.com>
2024-08-19 14:10:24 +0500

    initial commit

--- models/file.model.ts
+++ models/file.model.ts
@@ -3,7 +3,7 @@ import fileSchema, { IFile } from "../schemas/file.schema";
 
 fileSchema.pre<IFile>("save", async function (next) {
   const attachmentExists = await mongoose
-    .model("File")
+    .model("Attachment")
     .exists({ _id: this.attachmentRef });
   if (!attachmentExists) {
     throw new Error("Referenced attachment does not exist");
