aa933d09efaefbc95e1b4965f2900bd9da47e08d
Hamza <hamzamansoora@gmail.com>
2024-09-06 12:51:02 +0500

    initial commit

--- middleware/validation.middleware.ts
+++ middleware/validation.middleware.ts
@@ -3,9 +3,11 @@ import Joi from "joi";
 
 const validate = (schema: Joi.ObjectSchema) => {
   return (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body)
     const { error } = schema.validate(req.body, { abortEarly: false });
 
     if (error) {
+      console.log(error)
       return res.status(400).json({ message: error.message });
     }
     next();
